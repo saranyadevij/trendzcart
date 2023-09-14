a21f5073c7e47331c755c0108cff1994f1af0737
saran <48084964+saranyadevij@users.noreply.github.com>
2023-09-14 23:28:33 +0530

    my first e-commerce commit

---
+++ src/products.js
@@ -0,0 +1,38 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.webp";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.webp";
+
+export const PRODUCTS = [
+ {
+  id: 1,
+  productName: "Iphone",
+  price: 999.0,
+  productImage: product1,
+ },
+ {
+  id: 2,
+  productName: "Headset",
+  price: 122.0,
+  productImage: product2,
+ },
+ {
+  id: 3,
+  productName: "phone",
+  price: 520.0,
+  productImage: product3,
+ },
+ {
+  id: 4,
+  productName: "Iphone",
+  price: 199.0,
+  productImage: product4,
+ },
+ {
+  id: 5,
+  productName: "Iphone",
+  price: 999.0,
+  productImage: product5,
+ },
+];
